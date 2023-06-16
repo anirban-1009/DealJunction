1dbf623c9f28ef33b3ec09b802b1d5e25aa0d662
Anirban Sikdar <anirban9293@gmail.com>
2023-06-16 21:08:55 +0530

    Updated UI

--- ZapBuy/user/forms.py
+++ ZapBuy/user/forms.py
@@ -5,16 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    date_of_birth = forms.DateField()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['first_name'].required = True
-        self.fields['last_name'].required = True
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['first_name'].required = True
+    #     self.fields['last_name'].required = True
     
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth',  'password1', 'password2']
+        fields = ['username', 'email',  'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
