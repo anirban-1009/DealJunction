e9e0496176228ed496993b6c0b77ed504c1a89ee
Anirban Sikdar <anirban9293@gmail.com>
2023-06-18 13:02:31 +0530

    Added bit of UI and login decorator

--- django_auth/marketplace/models.py
+++ django_auth/marketplace/models.py
@@ -5,6 +5,7 @@ from users.models import User
 class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    image_url = models.URLField(null=True)
 
     def __str__(self):
         return self.name
