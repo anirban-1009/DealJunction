0470a57e06cb35518dc8fa9dfd624e8e7bf18dae
Anirban Sikdar <anirban9293@gmail.com>
2023-06-18 12:16:11 +0530

    binded creating user with creating cart

--- django_auth/users/views.py
+++ django_auth/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from marketplace.models import Cart
+from .models import User
 
 
 def register(request):
@@ -10,6 +12,9 @@ def register(request):
         if form.is_valid():
             form.save()
             email = form.cleaned_data.get('email')
+            user = User.objects.get(email__exact=email)
+            cart = Cart(user=user)
+            cart.save()
             messages.success(request, f'Account created for {email}! now login')
             return redirect('login')
 
