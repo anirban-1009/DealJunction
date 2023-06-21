0206a2634a7093f0892a74e20004fdbf96b97eeb
Anirban Sikdar <anirban9293@gmail.com>
2023-06-21 20:02:15 +0530

    Dynamic route done

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from .views import home
+from .views import home, buy_item
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('<int:id>/', buy_item)
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 from django.contrib.auth.decorators import login_required
+from users.models import User
+from .models import Cart
 
 @login_required
 def home(request):
     items = Item.objects.all()
     return render(request, 'marketplace/home.html', {'items': items})
+
+@login_required
+def buy_item(request, id):
+    current_user = request.user.id
+    try:
+        cart=Cart.objects.filter(id=current_user)
+        print(cart.all)
+    except User.DoesNotExist:
+        print("User Not found")
+    
+    return redirect('home')
